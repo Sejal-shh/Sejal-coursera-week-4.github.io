01467d4f5596446988df859560057f2a5d6b10cb
Sejal-shh <92288027+Sejal-shh@users.noreply.github.com>
2021-11-06 00:29:44 +0530

    Add files via upload

--- Script.js
+++ Script.js
@@ -1,14 +1,14 @@
 var names = new Array();
-names[0] = "Yaman";
+names[0] = "Yaakov";
 names[1] = "John";
-names[2] = "Jenny";
-names[3] = "jack";
-names[4] = "pranjal";
-names[5] = "frenny";
-names[6] = "lavanya";
-names[7] = "paran";
-names[8] = "sakshi";
-names[9] = "jimmy";
+names[2] = "Jen";
+names[3] = "jason";
+names[4] = "paul";
+names[5] = "frank";
+names[6] = "larry";
+names[7] = "paula";
+names[8] = "laura";
+names[9] = "jim";
 
 
 for (var i = 0 ; i < names.length; i++) {
